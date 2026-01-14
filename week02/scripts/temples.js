487fd3ff27d7e19417764df6e71ede86f962abc9
fpbarros63 <bar24094@byui.edu>
2026-01-13 21:18:14 -0300

    added temples.html, temples.css, temples-large.css, temples.js files in week02

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -3,5 +3,6 @@ const navList = document.querySelector(".nav-list");
 
 menuButton.addEventListener("click", () => {
   navList.classList.toggle("open");
-  menuButton.classList.toggle("open");
+    menuButton.classList.toggle("open");
+    
 });
